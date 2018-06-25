7579f82ffdd0461d4705f4fed3a5e7fc65105038
Comollo <comottofederico@gmail.com>
2018-06-25 11:21:57 +0200

    aggiornamenti

--- mongoexport-all-collections-as-json.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DB=$1
-COLLECTIONS=$(mongo localhost:27017/$DB --quiet --eval "db.getCollectionNames()" | sed 's/,/ /g')
-
-for collection in $COLLECTIONS; do
-    echo "Exporting $DB/$collection ..."
-    mongoexport -d newtickettoolDB -c $collection -o $collection.json
-done
